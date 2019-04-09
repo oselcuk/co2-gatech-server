91ffadeffc7106d4c7491f1af21964457f9970e7
Oguzhan Selcuk <oselcuk@outlook.com>
2019-04-09 18:24:46 -0400

    Add Google App Engine files for deployment

--- api.py
+++ api.py
@@ -62,7 +62,8 @@ def get_department(
     pass
 
 
+app = connexion.App(__name__, specification_dir='.', debug=True)
+app.add_api('api.yaml')
+
 if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir='.', debug=True)
-    app.add_api('api.yaml')
     app.run(port=80)
