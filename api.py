289883f2f46f2401a19e9c1f2d5c71dc83a51b8a
Oguzhan Selcuk <oselcuk@outlook.com>
2019-04-09 14:38:15 -0400

    Add OpenAPI specs for the api. Implement `/departments` endpoint to serve real data from FireStore
    TODO: /departments currently only respects `units`, `begin` and `end` query parameters, rest will need to be implemented

--- api.py
+++ api.py
@@ -1,37 +1,68 @@
-from flask import Flask, request, jsonify
-import json
+from datetime import datetime
 
-app = Flask(__name__)
-with open('dummy-data.json') as dd:
-    dummy_data = json.load(dd)
-    departments = []
-    for dep, details in dummy_data['departments'].items():
-        departments.append(details)
-        departments[-1]['url'] = '/departments/' + dep
+import connexion
+from google.cloud import firestore
 
-deps_base = '/departments'
-@app.route(deps_base, methods=['GET'])
-def get_deps():
-    args = request.args
-    #TODO: validate args
-    #TODO: use an argument parsing library for this
-    total = len(departments)
-    fields = args.get('fields')
-    print (fields)
-    offset = int(args.get('offset', 0))
-    limit = int(args.get('limit', 10))
-    limit = min(limit, 100, total - offset)
-    result = departments[offset:limit + offset] if limit > 0 else []
-    if fields:
-        fields = fields.split(',')
-        result = [{k:v for k,v in res.items() if k in fields}
-                    for res in result]
-    response = {
-        'total': total,
-        'ok': True,
-        'result': result
+db = firestore.Client.from_service_account_json('co2-gatech-service-key.json')
+
+
+def parse_date(date):
+    return datetime.strptime(date, '%Y-%m-%d') if date else None
+
+
+def department_to_dict(dep):
+    d = dep.to_dict()
+    d['department'] = dep.id
+    return d
+
+
+def list_departments(offset, limit, fields, units, begin=None, end=None):
+    # TODO: Respect the arguments when we have more than one department
+    kTotalEmissions = 'total_emissions'
+    kTotalDistance = 'total_distance'
+    kDepartment = 'department'
+
+    dep_refs = db.collection('departments').get()
+    deps = list(map(department_to_dict, dep_refs))
+    if begin or end:
+        begin = parse_date(begin) or datetime.min
+        end = parse_date(end) or datetime.max
+        relevant_flights = db.collection('flights').where(
+            'departure_date', '>=', begin
+        ).where('departure_date', '<=', end).get()
+        sums = {dep[kDepartment]: [0, 0] for dep in deps}
+        for flight in relevant_flights:
+            d = flight.to_dict()
+            s = sums[d[kDepartment].id]
+            s[0] += d['emission']
+            s[1] += d['distance']
+        for dep in deps:
+            dep[kTotalEmissions], dep[kTotalDistance] = sums[dep[kDepartment]]
+
+    if units == 'imperial':
+        for dep in deps:
+            # kg to lb
+            dep[kTotalEmissions] *= 2.20462
+            # km to mi
+            dep[kTotalDistance] *= 0.621371
+    return {
+        'result': deps,
+        'total': 1,
+        'timePeriod': '{}:{}'.format(begin, end)
     }
-    return jsonify(response)
+
+
+def add_department(idd, name):
+    return 404
+
+
+def get_department(
+    department, offset, limit, fields, units, period, granularity
+):
+    pass
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    app = connexion.App(__name__, specification_dir='.', debug=True)
+    app.add_api('api.yaml')
+    app.run(port=80)
